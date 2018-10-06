46207b278fe154d0e56ad0c8a2956dd3bef9e40d
aleksejmakajicodetribe <aleksej.makaji@codetri.be>
2018-10-06 15:28:20 +0200

    Init commit

---
+++ Listopia/app/src/main/java/com/aleksej/makaji/listopia/ListopiaApp.kt
@@ -0,0 +1,40 @@
+package com.aleksej.makaji.listopia
+
+import android.app.Activity
+import android.app.Application
+import android.app.Service
+import com.facebook.stetho.Stetho
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasActivityInjector
+import dagger.android.HasServiceInjector
+import timber.log.Timber
+import javax.inject.Inject
+import javax.inject.Singleton
+
+/**
+ * Created by Aleksej Makaji on 10/6/18.
+ */
+@Singleton
+class ListopiaApp : Application(), HasActivityInjector, HasServiceInjector {
+
+    @Inject
+    lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Activity>
+
+    @Inject
+    lateinit var serviceAndroidInjector: DispatchingAndroidInjector<Service>
+
+    override fun onCreate() {
+        super.onCreate()
+        //AppInjector.init(this)
+        if (BuildConfig.DEBUG) {
+            //Timber configuration
+            Timber.plant(Timber.DebugTree())
+            Stetho.initializeWithDefaults(this)
+        }
+    }
+
+    override fun activityInjector() = dispatchingAndroidInjector
+
+    override fun serviceInjector(): AndroidInjector<Service> = serviceAndroidInjector
+}
